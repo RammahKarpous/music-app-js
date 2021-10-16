ca5c1dc7006f54ce4f4c565f5e41e4e23bd48a1d
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-10-16 10:17:49 +0100

    Added the sidebar to the project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+	<Sidebar />
+	<router-view />
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import Sidebar from "./components/sidebar/Sidebar.vue";
+
+export default {
+	components: {
+		Sidebar
+	}
 }
+</script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style lang="scss">
+body {
+	background: $black-100;
+	margin: 0;
 }
 </style>
---
+++ src/components/sidebar/Sidebar.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="sidebar"></div>
+</template>
+
+<script>
+
+export default {
+	name: "Test",
+	components: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.sidebar {
+	background: #1a1817;
+	width: $width;
+	height: 100vh;
+
+	display: grid;
+	grid-template-rows: $width 1fr ($width + 100px);
+}
+</style>
